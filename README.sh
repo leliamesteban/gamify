5bd697f7359ba106085c15fbd333a6b0e6846621
Liam Esteban Prince <leliam.esteban@gmail.com>
2020-04-07 18:18:07 +0200

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-if [[ $# -gt 0 ]]
-then
-  cat <<< "$(jq '.mode |= "window"' < app/settings.json)" > app/settings.json
-  echo "Running in" $(jq '.mode' < app/settings.json) "mode"
-  echo "Generating the HTML files\n"
-  npx eleventy && \
-  echo Running the application
-  netlify dev
-fi
+cat <<< "$(jq '.mode |= "window"' < app/settings.json)" > app/settings.json
+echo "Running in" $(jq '.mode' < app/settings.json) "mode"
+echo "Generating the HTML files\n"
+npx eleventy && \
+echo Running the application
+netlify dev
